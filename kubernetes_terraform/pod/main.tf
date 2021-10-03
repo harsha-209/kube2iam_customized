ae342497df0002b3d5f30a50a5aca7111cf3a5e6
Ubuntu <ubuntu@ip-172-31-6-214.ap-south-1.compute.internal>
2021-10-03 15:05:21 +0000

    kube2iam_setup

---
+++ kubernetes_terraform/pod/main.tf
@@ -0,0 +1,32 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+
+
+
+
+
+
+
+resource "kubernetes_pod" "test" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 8080
+      }      
+    }
+  }
+}
